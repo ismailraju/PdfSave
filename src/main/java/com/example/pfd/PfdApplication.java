f638d1d5b393affb7cf4ad0ce4f03b35cb299276
raju <raju@istlbs.com>
2023-03-05 15:54:38 +0600

    first commit

---
+++ src/main/java/com/example/pfd/PfdApplication.java
@@ -0,0 +1,13 @@
+package com.example.pfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pfd/PfdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
